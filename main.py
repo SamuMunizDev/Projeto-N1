a44889cf6253a107142d6ca76f52585e4fa919f6
SamuMunizDev <89049526+SamuMunizDev@users.noreply.github.com>
2021-09-25 01:29:26 -0300

    🧨tentativa de facilitar o código✨
    Neste commit nós tentamos facilitar o código

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-sala = 1
-jogadas = 0
-LIMITE_JOGADAS = 7
-caminho = 1
-print('''Bem vindo ao game''')
-print('''Voce esta na sala: {}'''.format(sala))
-print('''Para iniciar voce deve escolher um dos dois caminhos: [1] vermelho ou [2] preto''')
-
-caminho = int(input("Escolha 1 para vermelho e 2 para preto:"))
-
+def emfrente
 if(caminho == 1):
     sala = sala + 1
     print("Voce esta na sala: {}".format(sala))
@@ -17,7 +8,4 @@ elif(caminho == 2):
         print("Voce esta na sala: {}".format(sala))
 else:
     print("Escolha Invalida")
-
-jogadas = jogadas + 1
-
-print("voce jogou {} vezes".format (jogadas))
+emfrente = int(input("Escolha 1 para vermelho e 2 para preto:"))
